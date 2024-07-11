f4968a1e755d14d18d9bc6bc111c42d598d04a99
Istabraq Kulow <istakulow@gmail.com>
2024-07-11 15:25:23 +0300

    stitching

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+
+   
+let testVar = {};
+require('./helpers.js');
+
+describe ("index.js", () => {
+  it("exists", () => {
+    expect(testVar).to.exist;
+    expect(testVar).to.deep.equal({}); 
+  });
+});
+
+
